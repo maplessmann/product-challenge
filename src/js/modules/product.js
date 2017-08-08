1a67d94593ba965dfad968d137028a3772419509
maplessmann <maplessmann@gmail.com>
2017-08-08 12:25:54 -0300

    Incluindo a altura do cano nos produtos

--- src/js/modules/product.js
+++ src/js/modules/product.js
@@ -12,6 +12,8 @@ const productModule = (function() {
 
         const toComma = (price) => price.toFixed(2).replace('.', ',');
 
+        const highTop = (isHigh) => isHigh ? 'Cano Alto' : 'Cano Baixo';
+
         data.forEach((product) => {
             const markup = `
                 <div class="col-xs-12 col-sm-6 col-md-3">
@@ -20,7 +22,7 @@ const productModule = (function() {
                             <img class="product-image" src="${product.image}" alt="Imagem de demonstração da chuteira">
                         </a>
                         <div class="product-title">${product.title}</div>
-                        <div class="product-category">${product.category}</div>
+                        <div class="product-top">${highTop(product['high-top'])}</div>
                         <div class="product-price">
                             <span class="price-normal">R$ ${toComma(product.price)}</span>
                             <span class="price-quota">ou 10X ${toComma(product.price / 10 - 0.09)} sem juros</span>
