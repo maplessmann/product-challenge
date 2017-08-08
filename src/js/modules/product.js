cf30fedfd3f9c1868b6a5cc5eb5cc0e2ddff2bd3
Matheus Plessmann <maplessmann@gmail.com>
2017-08-08 01:30:07 -0300

    Estruturando o layout

--- src/js/modules/product.js
+++ src/js/modules/product.js
@@ -8,20 +8,22 @@ const productModule = (function() {
     }
 
     function renderProducts(data, groupClass) {
-        const productsGroup = document.querySelector(`.products-group.products-${groupClass} .slider`);
+        const productsGroup = document.querySelector(`.products.products-${groupClass} .slider`);
+
+        const toComma = (price) => price.toFixed(2).replace('.', ',');
 
         data.forEach((product) => {
             const markup = `
                 <div class="col-xs-12 col-sm-6 col-md-3">
                     <div class="product" data-high-top="${product['high-top']}">
-                        <a class="product-link">
+                        <a href="#" class="product-link">
                             <img class="product-image" src="${product.image}" alt="Imagem de demonstração da chuteira">
                         </a>
                         <div class="product-title">${product.title}</div>
                         <div class="product-category">${product.category}</div>
                         <div class="product-price">
-                            <span class="normal">R$${product.price.toFixed(2)}</span>
-                            <span class="parcel">ou 10X ${(product.price / 10 - 0.09).toFixed(2)} sem juros</span>
+                            <span class="price-normal">R$ ${toComma(product.price)}</span>
+                            <span class="price-quota">ou 10X ${toComma(product.price / 10 - 0.09)} sem juros</span>
                         </div>
                         <a href="#" class="btn">Comprar</a>
                     </div>
